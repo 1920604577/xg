1b573f104ccf88bad24871e85819395940d699d9
hln <1920604577@qq.com>
2024-05-29 18:21:46 +0800

    test 2024-5-29-18:21 hln

---
+++ gy/src/main/java/com/hln/GyApplication.java
@@ -0,0 +1,11 @@
+package com.hln;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GyApplication.class, args);
+    }
+}
